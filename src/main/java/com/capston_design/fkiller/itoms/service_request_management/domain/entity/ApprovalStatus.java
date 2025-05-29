c622e7306dea19646c6cc716c4bfd9f3cd75ab30
memymini <minhi0614@gmail.com>
2025-05-29 16:35:38 +0900

    feat: entity 생성

---
+++ src/main/java/com/capston_design/fkiller/itoms/service_request_management/domain/entity/ApprovalStatus.java
@@ -0,0 +1,6 @@
+package com.capston_design.fkiller.itoms.service_request_management.domain.entity;
+
+public enum ApprovalStatus {
+    PENDING, APPROVED, REJECTED
+}
+
---
+++ src/main/java/com/capston_design/fkiller/itoms/service_request_management/domain/entity/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.capston_design.fkiller.itoms.service_request_management.domain.entity;
+
+public enum TaskStatus {
+    CREATED, IN_PROGRESS, COMPLETED, CANCELLED
+}
