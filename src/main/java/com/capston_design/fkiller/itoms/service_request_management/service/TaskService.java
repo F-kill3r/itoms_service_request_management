554c6f0fa51cfaf189ffaa4e6a49a6319639f459
dduddo <ch8930@naver.com>
2025-06-02 19:55:21 +0900

    feat: 태스크 상태 변경
    - 태스크 상태 변경 시, DB 업데이트 수행
    - 태스크 상태 변경 시, ticketCore에 callback url 요청
    - client 요청은 이벤트 형식으로 처리함
    ➕ callback url은 버전 path를 안넣어서 수정해야할 듯
    
    ⚠️ call back url의 경우에는 FK-58 작업인데, FK-57에서 같이 작업해버림...🫥

--- src/main/java/com/capston_design/fkiller/itoms/service_request_management/service/TaskService.java
+++ src/main/java/com/capston_design/fkiller/itoms/service_request_management/service/TaskService.java
@@ -2,8 +2,13 @@ package com.capston_design.fkiller.itoms.service_request_management.service;
 
 import com.capston_design.fkiller.itoms.service_request_management.controller.dto.request.CreateTasksRequestDTO;
 import com.capston_design.fkiller.itoms.service_request_management.controller.dto.response.TaskResponseInformation;
+import com.capston_design.fkiller.itoms.service_request_management.controller.dto.response.UpdateTaskStatusResponseDTO;
+
+import java.util.UUID;
 
 
 public interface TaskService {
     TaskResponseInformation createTask(CreateTasksRequestDTO request);
+    UpdateTaskStatusResponseDTO updateTaskStatus(UUID taskId, String taskStatus, String updatedTime);
+
 }
--- src/main/java/com/capston_design/fkiller/itoms/service_request_management/service/TaskServiceImpl.java
+++ src/main/java/com/capston_design/fkiller/itoms/service_request_management/service/TaskServiceImpl.java
@@ -1,14 +1,20 @@
 package com.capston_design.fkiller.itoms.service_request_management.service;
 
+import com.capston_design.fkiller.itoms.service_request_management.common.exception.BaseException;
 import com.capston_design.fkiller.itoms.service_request_management.controller.dto.request.CreateTasksRequestDTO;
 import com.capston_design.fkiller.itoms.service_request_management.controller.dto.response.TaskResponseInformation;
-import com.capston_design.fkiller.itoms.service_request_management.domain.entity.ticket_information.TicketInformation;
+import com.capston_design.fkiller.itoms.service_request_management.controller.dto.response.UpdateTaskStatusResponseDTO;
+import com.capston_design.fkiller.itoms.service_request_management.domain.entity.task.Task;
+import com.capston_design.fkiller.itoms.service_request_management.domain.entity.task.TaskStatus;
 import com.capston_design.fkiller.itoms.service_request_management.domain.task.TaskCreator;
 import com.capston_design.fkiller.itoms.service_request_management.domain.task.TaskDomain;
 import com.capston_design.fkiller.itoms.service_request_management.domain.task.TaskMapper;
 import com.capston_design.fkiller.itoms.service_request_management.domain.ticket.TicketDomain;
 import com.capston_design.fkiller.itoms.service_request_management.repository.TaskRepository;
+import com.capston_design.fkiller.itoms.service_request_management.service.dto.TaskStatusUpdateEvent;
+import com.capston_design.fkiller.itoms.service_request_management.service.event.rest.RestTaskStausUpdateEventListener;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +26,7 @@ import java.util.UUID;
 public class TaskServiceImpl implements TaskService{
 
     private final TicketInformationService ticketInformationService;
+    private final RestTaskStausUpdateEventListener restTaskStausUpdateEventListener;
     private final TaskRepository taskRepository;
     private final TaskMapper taskMapper;
     private final TaskCreator taskCreator;
@@ -31,5 +38,22 @@ public class TaskServiceImpl implements TaskService{
         TaskDomain taskDomain = taskCreator.createInitTaskDomain(request.getTaskName(), ticketDomain);
         UUID taskId = taskRepository.save(taskMapper.toEntity(taskDomain)).getId();
         return taskMapper.toTaskResponseInformation(taskId, taskDomain);
+    }
+
+    @Transactional
+    @Override
+    public UpdateTaskStatusResponseDTO updateTaskStatus(UUID task_id, String taskStatus, String updatedTime) {
+        Task task = taskRepository.findById(task_id)
+                .orElseThrow(() -> BaseException.createBaseExceptionWithoutDetail(HttpStatus.BAD_REQUEST,
+                        "유효하지 않은 Task ID 입니다."));
+        task.updateTaskStatus(TaskStatus.fromCodeName(taskStatus));
+        restTaskStausUpdateEventListener.handleTaskStatusUpdateEvent(
+                new TaskStatusUpdateEvent(task_id, task.getTicketId(), task.getTaskName(), taskStatus, updatedTime));
+        Task updateTask = taskRepository.save(task);
+
+        return new UpdateTaskStatusResponseDTO(
+                updateTask.getId(),
+                updateTask.getTaskStatus()
+        );
     }
 }
